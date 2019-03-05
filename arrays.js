af40611b11c75cc43c6de8712bb7db9ee0240f48
Ayaa <ayaa1siddig5@gmail.com>
2019-03-05 07:18:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'] ;
 
 function addElementToBeginningofArray(array, element){
-  array.push(element)
+  return [element,... array]
 }
